aebe3cdf1a66f427364a403e6454d5b82e850c47
Maverick Tesini <mavericktesini@gmail.com>
2018-07-22 05:09:10 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,7 +41,7 @@ class ApplicationController < Sinatra::Base
 
   post "/sessions" do #login and redirect
     @user = User.find_by(username: params["username"], password: params["password"])
-    if @user && @user.authenticate(params["password"])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect :"/users/home"
     else
