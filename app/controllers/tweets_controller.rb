1dd3ea9f06dbc4f8a8151252447098387d24662d
Maverick Tesini <mavericktesini@gmail.com>
2018-07-22 03:21:21 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,12 +6,20 @@ class TweetsController < ApplicationController
 
   post '/tweets' do #create tweet and redirect
     @tweet = Tweet.create(params[:tweet])
-    redirect :"/tweets/#{tweet.id}"
+    redirect :"/tweets/#{@tweet.id}"
   end
 
   get "/tweets/:id" do #single tweet page request
     @tweet = Tweet.find_by_id(params[:id])
-    erb :"/tweets/#{tweet.id}"
+    erb :"/tweets/#{@tweet.id}"
   end
 
+  get "/tweets/:id/edit" do #edit tweet page request 
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :"/tweets/edit"
+  end 
+
+  patch '/tweets' do #update tweet 
+
+    redirect "/tweets/#{@tweet.id}"
 end
